cafad26294346f34c8d5eb80b54ece0fdea758a1
LISIFEI <zcabsl0@ucl.ac.uk>
2024-01-15 23:19:54 +0000

    2

--- config/auditLogDBConfig.js
+++ config/auditLogDBConfig.js
@@ -7,6 +7,39 @@ const auditLogDBConfig = {
     database: 'IFRC',
 };
 
+
+const listAttributes = ['email', 'user', 'time', 'action', 'language', 'society'];
+
+// Symbol to separate elements in a list
+const newElementSymbol = '~|~';
+
+
+const AuditLogTableName = 'ifrc_Auditlog';
+
+
+
 const auditLogTableName = 'user_logs';
 
-module.exports = { auditLogDBConfig, auditLogTableName };
+module.exports = { auditLogDBConfig, auditLogTableName, listAttributes, AuditLogTableName, newElementSymbol};
+
+
+// pool.getConnection((err, connection) => {
+//     if (err) throw err;
+    
+//     const createTableQuery = `
+//         CREATE TABLE IF NOT EXISTS user_logs (
+//             email VARCHAR(255) AUTO_INCREMENT PRIMARY KEY,
+//             user INT,
+//             time DATETIME,
+//             action VARCHAR(255),
+//             language INT,
+//             society VARCHAR(255)
+//       )
+//     `;
+    
+//     connection.query(createTableQuery, (createTableErr, result) => {
+//       connection.release();
+//       if (createTableErr) throw createTableErr;
+//       console.log('user_logs table created successfully');
+//     });
+//   });
--- routes/AuditLog.js
+++ routes/AuditLog.js
@@ -1,20 +1,93 @@
-const express = require('express');
-const { logToAuditLog } = require('../controller/AuditLogController'); // 假设存在 AuditLogController 处理逻辑
-const { dbConfig, auditLogTableName, auditLogAttributes } = require('../config/auditLogDBConfig');
+const { selectFromDatabase } = require('../controllers/selectFromDatabase');
+const { updateToDatabase } = require('../controllers/updateToDatabase');
+const { deleteFromDatabase } = require('../controllers/deleteFromDatabase');
+var express = require('express'),
+    AuditLogRouter= express.Router();
+const { dbConfig, auditLogTableName, listAttributes } = require("../config/contentDBConfig");
 
-const router = express.Router();
 
-router.post('/log', async (req, res) => {
+AuditLogRouter.get('/', async (req, res) => {
     try {
-        const logEntry = req.body;
+        const { key, value } = req.query;
+        console.log('key: ', key);
+        console.log('value: ', value);
+        
+        var result = {};
 
-        await logToAuditLog(dbConfig, auditLogTableName, logEntry);
+        attribute = await selectFromDatabase(dbConfig, auditLogTableName, [], null, null) || {};
 
-        res.json({ success: true, message: 'Audit log entry successfully recorded.' });
+        contents = await selectFromDatabase(dbConfig, auditLogTableName, listAttributes, key, value) || {};
+
     } catch (error) {
-        console.error('Error:', error.message);
-        res.status(500).send('Internal Server Error');
+        if (!(error.code === 'ER_BAD_FIELD_ERROR')) {
+            console.error('Error:', error.message);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+    } finally{
+        console.log('attribute: ', attribute);
+        console.log('contents: ', contents);
+        result = { attribute: attribute, contents: contents };
+        res.json(result);
     }
 });
 
-module.exports = router;
+
+AuditLogRouter.post('/update', async (req, res) => {
+    try {
+        const object = req.body;
+        console.log('object: ', object);
+        await updateToDatabase(dbConfig, auditLogTableName, object);
+        res.json({ success: true, message: 'Data successfully inserted or updated.' });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+        console.error('Error:', error.message);
+    }
+});
+
+AuditLogRouter.post('/delete', async (req, res) => {
+    try {
+        const { key, value } = req.query;
+        console.log('key: ', key);
+        console.log('value: ', value);
+        var success = false;
+        
+        if (key && value && key !== ''){
+            await deleteFromDatabase(dbConfig, auditLogTableName, key, value);
+            success = true;
+            message = 'Data successfully deleted.';
+        }
+        else{
+            message = 'No key or value provided.';
+        }
+        res.json({ success: success, message: message});
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+        console.error('Error:', error.message);
+    }
+});
+
+AuditLogRouter.post('/select', async (req, res) => {
+    try {
+        const { key, value } = req.query;
+        console.log('key: ', key);
+        console.log('value: ', value);
+        var success = false;
+        var selectedData = {};
+
+        if (key && value && key !== ''){
+            selectedData = await selectFromDatabase(dbConfig, auditLogTableName, key, value);
+            success = true;
+            message = 'Data successfully selected.';
+        } else {
+            message = 'No key or value provided.';
+        }
+
+        res.json({ success: success, message: message, data: selectedData });
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+        console.error('Error:', error.message);
+    }
+});
+
+module.exports = AuditLogRouter;
