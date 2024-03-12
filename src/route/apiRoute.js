103664889406b016d3ae1f9451f00b1036df5dde
“Stephen <zcabyc1@ucl.ac.uk>
2024-03-12 09:45:11 +0000

    added auth() in apiRoute

--- src/route/apiRoute.js
+++ src/route/apiRoute.js
@@ -24,14 +24,14 @@ router.post('/delete_api_user', auth(), apiUserValidator.deleteApiUserValidator,
 
 // api routes
 
-router.post('/get_apis', apiController.getAllApis);
+router.post('/get_apis', auth(), apiController.getAllApis);
 
-router.post('/get_api_by_id', apiValidator.getApiByIdValidator, apiController.getApiById);
+router.post('/get_api_by_id', auth(), apiValidator.getApiByIdValidator, apiController.getApiById);
 
-router.post('/add_api', apiValidator.addApiValidator, apiController.addApi);
+router.post('/add_api', auth(), apiValidator.addApiValidator, apiController.addApi);
 
-router.post('/update_api', apiValidator.updateApiValidator, apiController.updateApi);
+router.post('/update_api', auth(), apiValidator.updateApiValidator, apiController.updateApi);
 
-router.post('/delete_api', apiValidator.deleteApiValidator, apiController.deleteApi);
+router.post('/delete_api', auth(), apiValidator.deleteApiValidator, apiController.deleteApi);
 
 module.exports = router;
